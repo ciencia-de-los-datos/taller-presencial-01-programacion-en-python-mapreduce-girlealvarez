63449114f4df85e4823d8c4dac3aedb3a8d127cd
girlealvarez <gialvarezg@unal.edu.co>
2024-02-23 20:12:27 -0500

    Update word_count.py
    Intento 1

--- word_count.py
+++ word_count.py
@@ -13,40 +13,92 @@
 #     ('text2.txt'. 'hypotheses.')
 #   ]
 #
-def load_input(input_directory):
-    pass
+import glob
+import fileinput
+import os.path # permite hacer operaciones de archivos
 
+def load_input(input_directory): #lee archivos y devuelve una lista de tuplas con el nombre del archivo y contenido del archivo
+    sequence = []
+    filenames = glob.glob(input_directory + "/*")  #"/*" muestra todo lo que hay en la carpeta input
+    with fileinput.input(files=filenames) as f: #abre archivos de la lista filenames
+        for line in f: # f variable con la que se puede iterar todos los archivos
+            sequence.append((fileinput.filename(), line)) #por cada iteracion se agrega una tupla a la lista
+    return sequence
 
-#
+# Otra forma de leer los archivos
+# def load_input(input_directory): # leer archivos en una carpeta
+
+#     dir_path = input_directory + "/*.txt"
+#     filenames = glob.glob(dir_path) 
+
+#     sequence = []
+#     with fileinput.input(files=filenames) as file:
+#         for line in file:
+#             tupla = (fileinput.filename(), line)
+#             sequence.append(tupla)
+#     return(sequence)
+
+# load_input ("input")
+
+#filenames = load_input("input")
+#print(filenames)
+#print (filenames[1]) #imprime el archivo solo con la primera línea
+    
 # Escriba una función llamada maper que recibe una lista de tuplas de la
 # función anterior y retorna una lista de tuplas (clave, valor). En este caso,
 # la clave es cada palabra y el valor es 1, puesto que se está realizando un
 # conteo.
-#
 #   [
-#     ('Analytics', 1),
+#     ('Analytics', 1), #Lista clave-valor. La clave es la palabra y el valor siempre es 1
 #     ('is', 1),
 #     ...
 #   ]
-#
-def mapper(sequence):
-    pass
+
+def mapper(sequence): #mapper convierte el contenido del archivo en una secuencia de palabras con sus ocurrencias
+    new_sequence = []
+    for _, text in sequence: #El guion bajo _ se utiliza para ignorar la primera parte de la tupla, que es el nombre del archivo, text contiene el contenido del archivo
+        words = text.split() #Divide el contenidio del archivo (text) en palabras utilizando split
+        for word in words: # Itera sobre todas las palabras en words
+            word = word.replace (",","")
+            word = word.replace (".","")
+            word = word.lower() # Deja todas las palabras en minúsculas
+            new_sequence.append((word,1)) #El 1 es la ocurrencia de la palabra
+    return new_sequence
+
+#Otra forma de hacer el mapper
+# def mapper (sequence):
+
+#     new_sequence = []
+#     for _, text in sequence:
+#         for word in text.split():
+#             tupla = (word, 1)
+#             new_sequence.append(tupla)
+#     return new_sequence      
 
 
-#
+#sequence = load_input("input")
+#sequence = mapper(sequence)
+#print (sequence)
+
 # Escriba la función shuffle_and_sort que recibe la lista de tuplas entregada
 # por el mapper, y retorna una lista con el mismo contenido ordenado por la
 # clave.
-#
 #   [
 #     ('Analytics', 1),
 #     ('Analytics', 1),
 #     ...
 #   ]
-#
-def shuffle_and_sort(sequence):
-    pass
+def shuffle_and_sort(sequence): #Ordena la secuencia de las palabras en orden alfabético
+    sorted_sequence= sorted(sequence, key=lambda x:x[0]) # X:X[0] extrae la primera letra de la palabara. 
+    #La secuencia se ordena según la primera letra de la palabra
+    #key paráametro que le pasa una función que retorna el elemento por el cual se hace el ordenamiento
+    #que es la primera parte de la tupla que es la palabra
+    return sorted_sequence
 
+# sequence= load_input("input")
+# sequence= mapper(sequence)
+# sequence= shuffle_and_sort(sequence)
+#print(sequence)
 
 #
 # Escriba la función reducer, la cual recibe el resultado de shuffle_and_sort y
@@ -54,16 +106,57 @@ def shuffle_and_sort(sequence):
 # ejemplo, la reducción indica cuantas veces aparece la palabra analytics en el
 # texto.
 #
-def reducer(sequence):
-    pass
+def reducer(sequence): 
+
+    diccionario={}
+    # for key, value in sequence: 
+    #     #print(key, "-->", value)
+    #     if key not in diccionario.keys(): # devuelve todas las claves del diccionario pero con not in el diccionario debe estar vacio
+    #         diccionario[key] = []
+    #     diccionario[key].append(value)
+    # #print(diccionario)
+        
+    # new_sequence = []
+    # for key, value in diccionario.items():
+    #     tupla = (key, sum(value))
+    #     new_sequence.append(tupla)
+    # return new_sequence
+
+    for key, value in sequence:
+        if key not in diccionario.keys():
+            diccionario[key] = 0  # En vez de arrancar con una lista, se empieza con cero
+        diccionario[key] += value
+    
+    new_sequence = []
+    for key, value in diccionario.items():
+        tupla = (key, value)
+        new_sequence.append(tupla)
+    
+    return new_sequence
+
+# sequence = load_input("input")
+# sequence= mapper(sequence)
+# sequence = shuffle_and_sort(sequence)
+# sequence =reducer(sequence)
+# print (sequence)
 
 
 #
 # Escriba la función create_ouptput_directory que recibe un nombre de directorio
 # y lo crea. Si el directorio existe, la función falla.
 #
-def create_ouptput_directory(output_directory):
-    pass
+
+def create_output_directory(output_directory):
+    
+    if os.path.exists(output_directory):
+        raise FileExistsError (f"The directory,'{output_directory}' alredy exist.") 
+    os.makedirs(output_directory)
+
+# sequence = load_input("input")
+# sequence= mapper(sequence)
+# sequence = shuffle_and_sort(sequence)
+# sequence =reducer(sequence)
+# create_ouptput_directory("ouput")
 
 
 #
@@ -75,7 +168,16 @@ def create_ouptput_directory(output_directory):
 # separados por un tabulador.
 #
 def save_output(output_directory, sequence):
-    pass
+    with open(output_directory + "/part-0000","w") as file:
+        for key, value in sequence:
+            file.write(f"{key}\t{value}\n")
+
+# sequence = load_input("input")
+# sequence= mapper(sequence)
+# sequence = shuffle_and_sort(sequence)
+# sequence =reducer(sequence)
+# create_output_directory("output")
+# save_output("output",sequence)
 
 
 #
@@ -83,18 +185,31 @@ def save_output(output_directory, sequence):
 # entregado como parámetro.
 #
 def create_marker(output_directory):
-    pass
+    with open (output_directory + "/SUCESS","w") as file:
+        file.write ("")
 
-
+# sequence = load_input("input")
+# sequence= mapper(sequence)
+# sequence = shuffle_and_sort(sequence)
+# sequence =reducer(sequence)
+# create_output_directory("output")
+# save_output("output",sequence)
+# create_marker("output")
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
 def job(input_directory, output_directory):
-    pass
+    sequence = load_input(input_directory)
+    sequence= mapper(sequence)
+    sequence = shuffle_and_sort(sequence)
+    sequence =reducer(sequence)
+    create_output_directory(output_directory)
+    save_output(output_directory,sequence)
+    create_marker(output_directory)
 
 
 if __name__ == "__main__":
-    job(
-        "input",
-        "output",
-    )
+     job(
+         "input",
+         "output",
+     )
